a2e68fd49e0f9f69cebfcc02594c7f2aa67b851c
skal48 <skal48@naver.com>
2023-11-14 12:46:03 +0900

    20231114sumin

--- mountain_tour/src/main/java/com/mountaintour/mountain/service/MagazineService.java
+++ mountain_tour/src/main/java/com/mountaintour/mountain/service/MagazineService.java
@@ -11,8 +11,10 @@ public interface MagazineService {
   
   public Map<String, Object> getMagazineList(HttpServletRequest request);
   public Map<String, Object> loadProductNo();
-  public int firstUpload(HttpServletRequest request);  
+  public Map<String, Integer> firstUpload(HttpServletRequest request);  
   public Map<String, Object> imageUpload(MultipartHttpServletRequest mnultipartRequest);
   public List<String> getEditorImageList(String contents);
   public boolean addThumbnail(MultipartHttpServletRequest multipartRequest) throws Exception;
+    
+  
 }
