171fbd9caad4ce6037630a194b8371c251d625d9
CHeessoo <gabycho41@gmail.com>
2023-11-16 09:24:52 +0900

    heesoo231115

--- mountain_tour/src/main/webapp/resources/sql/heesooQueryTest.sql
+++ mountain_tour/src/main/webapp/resources/sql/heesooQueryTest.sql
@@ -123,7 +123,7 @@ CREATE TABLE PRODUCT_T(
    PRODUCT_NO     NUMBER             NOT NULL,  -- 상품 번호 
    USER_NO        NUMBER             NOT NULL,  -- 작성자(관리자) 번호
    MOUNTAIN_NO    NUMBER             NOT NULL,  -- 산 번호
-   TRIP_NAME      VARCHAR2(150 BYTE) NOT NULL,  -- 여행이름(제목)
+   TRIP_NAME      VARCHAR2(255 BYTE) NOT NULL,  -- 여행이름(제목)
    TRIP_CONTENTS  CLOB               NULL,      -- 여행내용(설명)
    GUIDE          VARCHAR2(100 BYTE) NULL,      -- 가이드 정보
    TIMETAKEN      VARCHAR2(100 BYTE) NULL,      -- 여행일정(소요시간 ex 당일)
@@ -132,13 +132,13 @@ CREATE TABLE PRODUCT_T(
    REGISTERED_AT  DATE               NULL,      -- 등록일
    MODIFIED_DATE  DATE               NULL,      -- 수정일
    PEOPLE         NUMBER             NULL,      -- 최대인원수
-   HIT            NUMBER             NULL,      -- 조회수
+   HIT            NUMBER             DEFAULT 0, -- 조회수
    PLAN           VARCHAR2(255 BYTE) NULL,      -- 여행계획
    STATUS         NUMBER             NULL,      -- 상품상태 (예약가능:0, 예약불가:1)
-   TERM_USE       VARCHAR2(500 BYTE) NOT NULL,  -- 이용약관 (동의체크X, 약관내용을 DB에 저장해놓는 용도)
+   TERM_USE      VARCHAR2(500 BYTE)  NULL,      -- 이용약관 (동의체크X, 약관내용을 DB에 저장해놓는 용도)
    CONSTRAINT PK_PRODUCT PRIMARY KEY(PRODUCT_NO),
    CONSTRAINT FK_USER_PRODUCT     FOREIGN KEY(USER_NO)     REFERENCES USER_T(USER_NO)         ON DELETE CASCADE,
-   CONSTRAINT FK_MOUNTAIN_PRODUCT FOREIGN KEY(MOUNTAIN_NO) REFERENCES MOUNTAIN_T(MOUNTAIN_NO) ON DELETE SET NULL
+   CONSTRAINT FK_MOUNTAIN_PRODUCT FOREIGN KEY(MOUNTAIN_NO) REFERENCES MOUNTAIN_T(MOUNTAIN_NO) ON DELETE CASCADE
 );
 
 
@@ -147,7 +147,7 @@ CREATE TABLE PRODUCT_T(
 CREATE TABLE IMAGE_T(
     IMAGE_PATH        VARCHAR2(300 BYTE)  NOT NULL,  -- 첨부 사진 경로
     FILESYSTEM_NAME   VARCHAR2(300 BYTE)  NOT NULL,  -- 저장 파일명
-    THUMBNAIL         NUMBER,                        -- 썸네일이미지
+    THUMBNAIL         NUMBER              NULL,      -- 썸네일이미지
     PRODUCT_NO        NUMBER              NOT NULL,  -- 상품 번호
     CONSTRAINT FK_PRODUCT_IMAGE FOREIGN KEY(PRODUCT_NO) REFERENCES PRODUCT_T(PRODUCT_NO) ON DELETE CASCADE
 );
@@ -157,32 +157,33 @@ CREATE TABLE HEART_T (
 	USER_NO	   NUMBER  NOT NULL,  -- 회원 번호
 	PRODUCT_NO NUMBER  NOT NULL,  -- 상품 번호
     CONSTRAINT FK_USER_HEART    FOREIGN KEY(USER_NO)    REFERENCES USER_T(USER_NO)       ON DELETE CASCADE,
-    CONSTRAINT FK_PRODUCT_HEART FOREIGN KEY(PRODUCT_NO) REFERENCES PRODUCT_T(PRODUCT_NO) ON DELETE SET NULL
+    CONSTRAINT FK_PRODUCT_HEART FOREIGN KEY(PRODUCT_NO) REFERENCES PRODUCT_T(PRODUCT_NO) ON DELETE CASCADE
 );
 
 
 --**********************************************************************************
-
 -- 매거진 테이블
 CREATE TABLE MAGAZINE_T (
-	MAGAZINE_NO NUMBER	           NOT NULL,  -- 매거진 번호
-	USER_NO	    NUMBER		       NOT NULL,  -- 회원 번호
-	TITLE	    VARCHAR2(100 BYTE) NOT NULL,  -- 매거진 제목
-	CONTENTS	CLOB		       NOT NULL,  -- 매거진 내용
-    CREATE_AT	DATE	           NULL,      -- 매거진 작성날짜
-	HIT	        NUMBER		       DEFAULT 0, -- 매거진 조회수
-    PRODUCT_NO	NUMBER		       NOT NULL,  -- 상품 번호
+    MAGAZINE_NO NUMBER	            NOT NULL,  -- 매거진 번호
+    USER_NO	    NUMBER		        NOT NULL,  -- 회원 번호
+    TITLE	    VARCHAR2(100 BYTE)  NOT NULL,  -- 매거진 제목
+    CONTENTS	CLOB		        NOT NULL,  -- 매거진 내용
+    SUMMARY     VARCHAR2(2000 BYTE) NULL,      -- (리스트용) 요약
+    HIT	        NUMBER		        DEFAULT 0, -- 매거진 조회수
+    CREATE_AT	DATE	            NULL,      -- 매거진 작성날짜
+    PRODUCT_NO	NUMBER		        NOT NULL,  -- 상품 번호
     CONSTRAINT PK_MAGAZINE_NO PRIMARY KEY(MAGAZINE_NO),
-    CONSTRAINT FK_USER_MAGAZIN FOREIGN KEY(USER_NO) REFERENCES USER_T(USER_NO) ON DELETE SET NULL,
+    CONSTRAINT FK_USER_MAGAZIN    FOREIGN KEY(USER_NO) REFERENCES USER_T(USER_NO)          ON DELETE SET NULL,
     CONSTRAINT FK_PRODUCT_MAGAZIN FOREIGN KEY(PRODUCT_NO) REFERENCES PRODUCT_T(PRODUCT_NO) ON DELETE SET NULL
 );
 
 -- 매거진 멀티미디어
 CREATE TABLE MAGAZINE_MULTI_T (
-	MAGAZINE_NO	 NUMBER	       NOT NULL,  -- 매거진 번호
-	MULTI_PATH	 VARCHAR2(100 BYTE) NULL, -- 멀티미디어 경로
-	FILESYS_NAME VARCHAR2(100 BYTE) NULL, -- 파일 이름
-    CONSTRAINT FK_MAGAZINE_MULTI FOREIGN KEY(MAGAZINE_NO) REFERENCES MAGAZINE_T(MAGAZINE_NO) ON DELETE SET NULL  
+	MAGAZINE_NO	 NUMBER	            NULL,  -- 매거진 번호
+	MULTI_PATH	 VARCHAR2(100 BYTE) NULL,  -- 멀티미디어 경로
+	FILESYS_NAME VARCHAR2(100 BYTE) NULL,  -- 파일 이름
+    IS_THUMBNAIL NUMBER             NULL,  -- 썸네일로 쓰이는 사진 구분
+  CONSTRAINT FK_MAGAZINE_MULTI FOREIGN KEY(MAGAZINE_NO) REFERENCES MAGAZINE_T(MAGAZINE_NO) ON DELETE SET NULL  
 );
 
 -- 매거진 좋아요 
@@ -205,11 +206,10 @@ CREATE TABLE RESERVE_T (
     AGREE           NUMBER              NOT NULL,   -- 0:필수동의, 1:선택까지동의
     PICKUP_LOC      VARCHAR2(100 BYTE)  NULL,       -- 버스 탑승지
     RESERVE_STATUS  NUMBER              DEFAULT 0,  -- 0:정상, 1:대기, 2:불가
-    RESERVE_START   DATE                NULL,       -- 예약시작일
-    RESERVE_FINISH  DATE                NULL,       -- 예약종료일
+    RESERVE_START   VARCHAR2(30 BYTE)   NULL,       -- 예약시작일
     RESERVE_PERSON  NUMBER              NULL,       -- 예약한 총 인원수
-    PRODUCT_NO      NUMBER              NOT NULL,   -- 상품번호(FK)    
     USER_NO         NUMBER              NULL,       -- 유저번호(FK)
+    PRODUCT_NO      NUMBER              NOT NULL,   -- 상품번호(FK)    
     CONSTRAINT PK_RES PRIMARY KEY(RESERVE_NO),
     CONSTRAINT FK_USER_RES FOREIGN KEY(USER_NO) REFERENCES USER_T(USER_NO) ON DELETE SET NULL,
     CONSTRAINT FK_PRODUCT_RES FOREIGN KEY(PRODUCT_NO) REFERENCES PRODUCT_T(PRODUCT_NO) ON DELETE CASCADE
@@ -219,7 +219,7 @@ CREATE TABLE RESERVE_T (
 CREATE TABLE TOURIST_T (
     TOURIST_NO   NUMBER             NOT NULL,     -- 실제 여행객 번호(PK)
     NAME         VARCHAR2(100 BYTE) NULL,         -- 이름
-    BIRTH_DATE   DATE               NULL,         -- 생년월일
+    BIRTH_DATE   VARCHAR2(30 BYTE)  NULL,         -- 생년월일
     GENDER       VARCHAR2(2 BYTE)   NULL,         -- 성별
     CONTACT      VARCHAR2(30 BYTE)  NULL,         -- 연락처
     AGE_CASE     NUMBER             NULL,         -- 0:성인, 1:소아, 2:유아
@@ -227,6 +227,7 @@ CREATE TABLE TOURIST_T (
     CONSTRAINT PK_TOUR PRIMARY KEY(TOURIST_NO),
     CONSTRAINT FK_RES_TOUR FOREIGN KEY(RESERVE_NO) REFERENCES RESERVE_T(RESERVE_NO) ON DELETE CASCADE
 );
+
 
 -- 결제 테이블
 CREATE TABLE PAYMENT_T (
@@ -238,9 +239,8 @@ CREATE TABLE PAYMENT_T (
     PAY_DATE     VARCHAR2(100 BYTE) NULL,      -- 결제일
     RESERVE_NO   NUMBER             NULL,      -- 예약번호(FK)
     CONSTRAINT PK_PAY PRIMARY KEY(PAYMENT_NO),
-    CONSTRAINT FK_RES_PAY FOREIGN KEY(RESERVE_NO) REFERENCES RESERVE_T(RESERVE_NO) ON DELETE SET NULL
+    CONSTRAINT FK_RESERVE_PAY FOREIGN KEY(RESERVE_NO) REFERENCES RESERVE_T(RESERVE_NO) ON DELETE SET NULL
 );
-
 
 
 --*************************************상품 리뷰 테이블*********************************************
@@ -265,16 +265,15 @@ CREATE TABLE REVIEW_T (
 
 --**********************************************************************************
 
-
 -- 문의하기 테이블
 CREATE TABLE INQUIRY_T (
-    INQUIRY_NO       NUMBER             NOT NULL,           -- 문의번호         (PK)
-    USER_NO          NUMBER             NOT NULL,           -- 회원번호(작성자) (FK)
-    PRODUCT_NO       NUMBER             NULL,               -- 상품번호         (FK)
-    INQUIRY_TITLE    VARCHAR2(100 BYTE) NULL,               -- 제목
-    INQUIRY_CONTENTS CLOB               NULL,               -- 내용
-    IP               VARCHAR2(30 BYTE)  NULL,               -- IP주소
-    CREATED_AT       DATE               NULL,               -- 작성일
+    INQUIRY_NO       NUMBER             NOT NULL,   -- 문의번호         (PK)
+    USER_NO          NUMBER             NOT NULL,   -- 회원번호(작성자) (FK)
+    PRODUCT_NO       NUMBER             NULL,       -- 상품번호         (FK)
+    INQUIRY_TITLE    VARCHAR2(100 BYTE) NULL,       -- 제목
+    INQUIRY_CONTENTS CLOB               NULL,       -- 내용
+    IP               VARCHAR2(30 BYTE)  NULL,       -- IP주소
+    CREATED_AT       DATE               NULL,       -- 작성일
     CONSTRAINT PK_INQUIRY PRIMARY KEY(INQUIRY_NO),
     CONSTRAINT FK_USER_INQUIRY FOREIGN KEY(USER_NO) REFERENCES USER_T(USER_NO) ON DELETE CASCADE,
     CONSTRAINT FK_PRODUCT_INQUIRY FOREIGN KEY(PRODUCT_NO) REFERENCES PRODUCT_T(PRODUCT_NO) ON DELETE SET NULL
@@ -294,10 +293,9 @@ CREATE TABLE INQUIRY_ANSWER_T (
     CONSTRAINT FK_USER_ANSWER FOREIGN KEY(USER_NO) REFERENCES USER_T(USER_NO) ON DELETE SET NULL
 );
 
-
 -- 자주묻는질문 테이블
 CREATE TABLE FAQ_T (
-    FAQ_NO      NUMBER             NOT NULL,   -- 글번호 (PK)
+    FAQ_NO      NUMBER             NOT NULL,   -- 글번호  (PK)
     TITLE       VARCHAR2(100 BYTE) NULL,       -- 제목
     CONTENTS    CLOB               NULL,       -- 내용
     CREATED_AT  DATE               NULL,       -- 작성일
@@ -317,6 +315,7 @@ CREATE TABLE NOTICE_T (
     MODIFIED_AT     DATE,                          -- 공지 수정일
     CONSTRAINT PK_NOTICE_T PRIMARY KEY(NOTICE_NO)
 ); 
+
 
 --**********************************************************************************
 
